adc04efa17c5e97862d4ca8328aa180f85fa99a0
SAY-Hong <bobseahee@gmail.com>
2024-01-25 01:21:59 +0900

    [Edit] 주석 추가#1

--- FinalHrmiProjects/Resource/Components/SearchBar.swift
+++ FinalHrmiProjects/Resource/Components/SearchBar.swift
@@ -24,7 +24,7 @@ struct SearchBar: View {
         .padding(EdgeInsets(top: 15, leading: 0, bottom: 15, trailing: 20))
         .background(Color(red: 0.46, green: 0.46, blue: 0.50).opacity(0.12))
         .cornerRadius(10)
-        .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+        .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)) // 오토레이아웃을 위한 padding
     }
 }
 
