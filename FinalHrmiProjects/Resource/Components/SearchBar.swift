d46761c479e12975575b02a80df0c123c0783f25
SAY-Hong <bobseahee@gmail.com>
2024-01-25 01:18:18 +0900

    [Feat] 프레임 구성

--- FinalHrmiProjects/Resource/Components/SearchBar.swift
+++ FinalHrmiProjects/Resource/Components/SearchBar.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct SearchBar: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            HStack(spacing: 0) {
+                Image(systemName: "magnifyingglass")
+                    .padding(.leading)
+                Text("검색")
+                    .font(.medium16)
+                    .lineSpacing(22)
+            }
+            Spacer()
+            Image(systemName: "xmark")
+        }
+        .foregroundColor(.gray01)
+        .padding(EdgeInsets(top: 15, leading: 0, bottom: 15, trailing: 20))
+        .background(Color(red: 0.46, green: 0.46, blue: 0.50).opacity(0.12))
+        .cornerRadius(10)
+        .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
     }
 }
 
