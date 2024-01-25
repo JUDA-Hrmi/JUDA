867d301e668e21f11ea758b43a114e08eda2493b
MJKim <qorhaqt@gmail.com>
2024-01-25 13:32:47 +0900

    [Design] #4 PostCell UI 코드 수정, #13 CustomTextSegment UI 코드 수정

--- FinalHrmiProjects/Resource/Components/CustomTextSegment.swift
+++ FinalHrmiProjects/Resource/Components/CustomTextSegment.swift
@@ -7,54 +7,59 @@
 
 import SwiftUI
 
+// PostView와 LikedView에서 세그먼트에서 사용될 문자열을 저장하는 namespace
 enum PostOrLiked {
-	case post, liked
-	
-	var segments: [String] {
-		switch self {
-		case .post:
-			return ["인기", "최신"]
-		case .liked:
-			return ["술상 리스트", "술찜 리스트"]
-		}
-	}
+	static let post = ["인기", "최신"]
+	static let liked = ["술상 리스트", "술찜 리스트"]
 }
 
 struct CustomTextSegment: View {
 	
 	let segments: [String]
-	@Binding var selected: String
-	@Namespace var name
+	@Binding var selectedSegmentIndex: Int
+	
+	@Namespace private var animation // 애니메이션을 주기위해 뷰에 대한 이름을 지정하는 속성 프로퍼티
+	private let id = "CustomTextSegment" // 애니메이션을 주고싶은 뷰에 대한 id값 지정
 	
 	var body: some View {
 		HStack(spacing: 20) {
-			ForEach(segments, id: \.self) { segment in
-				Button {
-					selected = segment
-				} label: {
-					VStack {
-						Text(segment)
-							.font(.medium14)
-							.foregroundColor(selected == segment ? .mainBlack : .gray01)
-						ZStack {
+			ForEach(0..<segments.count, id: \.self) { index in				
+				VStack {
+					// 세그먼트 텍스트
+					Text(segments[index])
+						.font(.medium14)
+						// 선택된 인덱스에 해당하는 문자열에 black, 아닌 문자열엔 gray
+						.foregroundColor(selectedSegmentIndex == index ? .mainBlack : .gray01)
+					
+					// 세그먼트 텍스트 하단 바
+					ZStack {
+						// 선택되지 않은 문자열 하단엔 바 안보임
+						Capsule()
+							.fill(.clear)
+							.frame(height: 3)
+						// 선택된 인덱스에 해당하는 문자열 하단에 바 보여짐
+						if selectedSegmentIndex == index {
 							Capsule()
-								.fill(Color.clear)
+								.fill(.mainBlack)
 								.frame(height: 3)
-							if selected == segment {
-								Capsule()
-									.fill(.mainBlack)
-									.frame(height: 3)
-									.matchedGeometryEffect(id: "Tab", in: name)
-							}
+								// 좌우 전환하는 과정에서 생기는 뷰 사이에 애니메이션을 주기위한 수정자 추가
+								.matchedGeometryEffect(id: id, in: animation)
 						}
+					}
+				}
+				.onTapGesture {
+					// 세그먼트 전환 시, 부드럽게 전환하기위한 애니메이션
+					withAnimation {
+						selectedSegmentIndex = index
 					}
 				}
 			}
 		}
+		// 텍스트의 width와 하단 바의 width 맞춰줌
 		.fixedSize()
 	}
 }
 
-//#Preview {
-//	CustomTextSegment()
-//}
+#Preview {
+	return CustomTextSegment(segments: PostOrLiked.liked, selectedSegmentIndex: .constant(0))
+}
--- FinalHrmiProjects/Resource/Components/PostCell.swift
+++ FinalHrmiProjects/Resource/Components/PostCell.swift
@@ -13,13 +13,15 @@ struct PostCell: View {
 	@Binding var likeCount: Int
 	
 	var body: some View {
-		VStack {
+		// VStack에 기본적인 spacing이 들어가기 때문에 0으로 설정
+		VStack(spacing: 0) {
 			ZStack(alignment: .topTrailing) {
+				// 게시글 사진리스트의 첫 번째 사진
 				Image("foodEx3")
 					.resizable()
 					.frame(height: 170)
-					.padding(.bottom, -8)
 				
+				// 게시글 사진이 2장 이상일 경우, 상자 아이콘이 사진의 trailing 상단에 보여짐
 				Image(systemName: "square.on.square.fill")
 					.frame(width: 18, height: 18)
 					.foregroundStyle(.white)
@@ -27,10 +29,13 @@ struct PostCell: View {
 			}
 			HStack {
 				HStack {
+					// 사용자의 프로필 사진
 					Image("appIcon")
 						.resizable()
 						.frame(width: 20, height: 20)
 						.clipShape(.circle)
+					
+					// 사용자의 닉네임
 					Text("hrmi")
 						.lineLimit(1)
 						.font(.regular14)
@@ -39,27 +44,32 @@ struct PostCell: View {
 				
 				Spacer()
 				
-				Button {
+				// 좋아요 버튼
+				HStack {
+					// 좋아요를 등록 -> 빨간색이 채워진 하트
+					// 좋아요를 해제 -> 테두리가 회색인 하트
+					Image(systemName: isLike ? "heart.fill" : "heart")
+						.foregroundStyle(isLike ? .mainAccent01 : .gray01)
+					Text("\(likeCount)")
+						.foregroundStyle(.gray01)
+				}
+				.font(.regular14)
+				.padding(.trailing, 10)
+				.onTapGesture {
 					likeButtonAction()
-				} label: {
-					HStack {
-						Image(systemName: isLike ? "heart.fill" : "heart")
-							.foregroundStyle(isLike ? .mainAccent01 : .gray01)
-						Text("\(likeCount)")
-							.foregroundStyle(.gray01)
-					}
-					.font(.regular14)
-					.padding(.trailing, 10)
 				}
 			}
 			.frame(height: 30)
-			.border(.black)
+			.border(.mainBlack)
 		}
 		.frame(width: 170, height: 200)
-		.border(.black)
+		.border(.mainBlack)
 	}
 	
+	// 좋아요 버튼 액션 메서드
 	private func likeButtonAction() {
+		// 좋아요 등록 -> 좋아요 수에 + 1
+		// 좋아요 해제 -> 좋아요 수에 - 1
 		if isLike {
 			likeCount -= 1
 		} else {
