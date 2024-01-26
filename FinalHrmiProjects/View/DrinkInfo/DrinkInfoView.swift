9b4b1125e0a594b32db0287208d31c5efaa0b1f3
Phangg <leeckdwns9305@gmail.com>
2024-01-26 14:32:51 +0900

    [Add] DrinkInfoView 세그먼트 외 디자인 #14

--- FinalHrmiProjects/View/DrinkInfo/DrinkInfoView.swift
+++ FinalHrmiProjects/View/DrinkInfo/DrinkInfoView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct DrinkInfoView: View {
+    // 세그먼트 선택 - isGridView ? 그리드 뷰 : 리스트 뷰
+    @State private var isGridView = true
+    // 술 그리드 셀 2개 column
+    private let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible())]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(spacing: 0) {
+                // 검색 창
+                SearchBar()
+                // 술 종류 가로 스크롤
+                // TODO: 각 탭의 결과에 맞춰 보여줄 리스트 변경 필요
+                DrinkSelectHorizontalScrollBar()
+                // 세로 스크롤
+                ScrollView {
+                    // 세그먼트 + 필터링
+                    
+                    if isGridView {
+                        // 그리드
+                        LazyVGrid(columns: columns, spacing: 10) {
+                            // TODO: 현재 더미데이터 10개를 보여주지만, 데이터 들어온 리스트로 ForEach 돌릴 예정
+                            ForEach(0..<10, id: \.self) { _ in
+                                // TODO: 추후에 네비게이션으로 해당 술의 Detail 로 이동 연결
+                                DrinkGridCell()
+                            }
+                        }
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 10)
+                    } else {
+                        // 리스트
+                        LazyVStack(spacing: 0) {
+                            // TODO: 현재 더미데이터 10개를 보여주지만, 데이터 들어온 리스트로 ForEach 돌릴 예정
+                            ForEach(0..<10, id: \.self) { _ in
+                                // TODO: 추후에 네비게이션으로 해당 술의 Detail 로 이동 연결
+                                DrinkListCell()
+                            }
+                        }
+                    }
+                }
+                // 스크롤 인디케이터 X
+                .scrollIndicators(.hidden)
+            }
+        }
     }
 }
 
