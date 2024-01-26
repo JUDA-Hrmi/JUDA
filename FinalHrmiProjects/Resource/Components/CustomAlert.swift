7d6235c29729cb9214ad3533db7c866d798770dd
백대홍 <bdh3620@naver.com>
2024-01-26 16:35:50 +0900

    Feat:CustomAlertView add #26

---
+++ FinalHrmiProjects/Resource/Components/CustomAlert.swift
@@ -0,0 +1,18 @@
+//
+//  CustomAlert.swift
+//  FinalHrmiProjects
+//
+//  Created by 백대홍 on 1/26/24.
+//
+
+import SwiftUI
+
+struct CustomAlert: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomAlert()
+}
