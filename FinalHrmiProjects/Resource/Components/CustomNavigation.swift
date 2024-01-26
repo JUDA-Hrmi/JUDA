816d3f7576e58292bbbbe48bf9448b2d49fdb33a
백대홍 <bdh3620@naver.com>
2024-01-26 23:39:18 +0900

    Add: extention View -> View + folder #3

--- FinalHrmiProjects/Resource/Components/CustomNavigation.swift
+++ FinalHrmiProjects/Resource/Components/CustomNavigation.swift
@@ -67,30 +67,7 @@ struct WillDisappearModifier: ViewModifier {
     }
 }
 
-// MARK: - View +
 
-extension View {
-    
-    // 뷰가 사라질 때 실행할 클로저를 받아 "onWillDisappear" 모디파이어를 추가하는 메서드
-    func onWillDisappear(_ perform: @escaping () -> Void) -> some View {
-        self.modifier(WillDisappearModifier(callback: perform))
-    }
-    
-    // 사용자 지정 네비게이션 바를 처리하는 커스텀 네비게이션바 Modifier
-    func customNavigationBar<C, L, T> (
-        centerView: @escaping (() -> C) = { EmptyView() },
-        leadingView: @escaping (() -> L),
-        trailingView: [NavigationTrailingButtonPostion: () -> T] = [:]
-    ) -> some View where C: View, L: View, T: View {
-        modifier(
-            CustomNavigationBarModifier(
-                centerView: centerView,
-                leadingView: leadingView,
-                trailingViews: trailingView
-            )
-        )
-    }
-}
 
 // MARK: - CustomNavigationBarModifier
 enum NavigationTrailingButtonPostion: Hashable, Comparable {
