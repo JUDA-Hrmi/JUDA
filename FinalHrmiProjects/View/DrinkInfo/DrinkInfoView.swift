69b289982a8ad82a9c7037d8a2007320823d818c
Phangg <leeckdwns9305@gmail.com>
2024-01-28 17:08:53 +0900

    [Add] DrinkInfoView - 선택된 술 타입에 따라 view 페이징처럼 보이도록 수정 #14

--- FinalHrmiProjects/View/DrinkInfo/DrinkInfoView.swift
+++ FinalHrmiProjects/View/DrinkInfo/DrinkInfoView.swift
@@ -13,23 +13,49 @@ struct DrinkInfoView: View {
     @State private var selectedSortingOption: String = "인기순"
     // 세그먼트 선택 - isGridView ? 그리드 뷰 : 리스트 뷰
     @State private var isGridView = true
-    
+    // UITest - Drink 종류 DummyData
+    private let typesOfDrink = [
+        "전체", "우리술", "맥주", "위스키", "와인", "브랜디", "리큐르", "럼", "사케", "기타"
+    ]
+    @State private var selectedDrinkTypeIndex = 0
+
+
     var body: some View {
         NavigationStack {
             ZStack {
                 VStack(spacing: 0) {
                     // 검색 창
                     SearchBar()
-                    // 술 종류 가로 스크롤
-                    // TODO: 각 탭의 결과에 맞춰 보여줄 리스트 변경 필요
-                    DrinkSelectHorizontalScrollBar()
-                    // 세그먼트 + 필터링
-                    DrinkInfoSegment(optionNameList: optionNameList, selectedSortingOption: $selectedSortingOption, isShowingSheet: $isShowingSheet, isGridView: $isGridView)
-                    // 그리드
-                    if isGridView {
-                        DrinkInfoGrid()
-                    } else {
-                        DrinkInfoList()
+                    ScrollViewReader { value in
+                        Group {
+                            // 술 종류 가로 스크롤
+                            // TODO: 각 탭의 결과에 맞춰 보여줄 리스트 변경 필요
+                            DrinkSelectHorizontalScrollBar(selectedDrinkTypeIndex: $selectedDrinkTypeIndex)
+                            
+                            // 세그먼트 + 필터링
+                            DrinkInfoSegment(optionNameList: optionNameList, selectedSortingOption: $selectedSortingOption, isShowingSheet: $isShowingSheet, isGridView: $isGridView)
+                            
+                            // 술 뷰 - DrinkSelectHorizontalScrollBar 의 선택에 따라 자연스럽게 페이징으로 화면 전환
+                            TabView(selection: $selectedDrinkTypeIndex) {
+                                // TODO: 각 술 타입에 맞는 리스트를 grid 와 list 에 뿌려줘야 함
+                                ForEach(0..<typesOfDrink.count, id: \.self) { _ in
+                                    // 그리드
+                                    if isGridView {
+                                        DrinkInfoGrid()
+                                    // 리스트
+                                    } else {
+                                        DrinkInfoList()
+                                    }
+                                }
+                            }
+                            .tabViewStyle(.page(indexDisplayMode: .never))
+                        }
+                        .onChange(of: selectedDrinkTypeIndex) { newValue in
+                            withAnimation {
+                                value.scrollTo(newValue, anchor: .top) // 화면 상단 고정
+                            }
+                        }
+                        .animation(.spring, value: selectedDrinkTypeIndex)
                     }
                 }
                 // DrinkInfoSegment 클릭 시 띄워지는 CustomSheet뷰
