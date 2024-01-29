e105ac8715c033fb3e8fde1af8590d59e8dd5c1b
SAY-Hong <bobseahee@gmail.com>
2024-01-29 22:39:05 +0900

    [Add] WebKit 추가 및 적용 #35

---
+++ FinalHrmiProjects/Resource/WebView.swift
@@ -0,0 +1,18 @@
+//
+//  WebView.swift
+//  FinalHrmiProjects
+//
+//  Created by 홍세희 on 2024/01/29.
+//
+
+import SwiftUI
+
+struct WebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebView()
+}
