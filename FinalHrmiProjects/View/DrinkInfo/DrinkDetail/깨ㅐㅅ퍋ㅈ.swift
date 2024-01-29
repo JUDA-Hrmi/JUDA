602ae56c58e89b7848d950a15e22dc80b2eb8318
Phangg <leeckdwns9305@gmail.com>
2024-01-29 14:22:24 +0900

    [Add] DrinkDetailView - 커스텀 네비게이션 적용 #15

---
+++ FinalHrmiProjects/View/DrinkInfo/DrinkDetail/깨ㅐㅅ퍋ㅈ.swift
@@ -0,0 +1,18 @@
+//
+//  깨ㅐㅅ퍋ㅈ.swift
+//  FinalHrmiProjects
+//
+//  Created by phang on 1/29/24.
+//
+
+import SwiftUI
+
+struct ________: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ________()
+}
