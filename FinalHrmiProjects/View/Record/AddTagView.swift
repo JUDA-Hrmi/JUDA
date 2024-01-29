97dd05ce173913bfe09f4376484043fd3a46b54b
withseon <dlstjsvv@gmail.com>
2024-01-29 22:25:52 +0900

    [Add] 술 태그 추가하기 Button 구현

--- FinalHrmiProjects/View/Record/AddTagView.swift
+++ FinalHrmiProjects/View/Record/AddTagView.swift
@@ -11,6 +11,8 @@ struct AddTagView: View {
     // TODO: 데이터 타입 변경 필요
     // 이미지 배열
     @State private var selectedPhotos: [UIImage?] = Array(repeating: nil, count: 10)
+    // SearchTagView sheet 프로퍼티
+    @State private var isShowTagSearch = false
 
     var body: some View {
         VStack {
@@ -18,6 +20,26 @@ struct AddTagView: View {
             // TODO: sheet로 올라오는 photopicker에 선택된 사진 체크 처리 및 이미지 뷰 수정
             ImageSelectHorizontalScrollView(selectedPhotos: $selectedPhotos)
                 .padding(.horizontal, 20)
+            
+            // 술 태그 추가 버튼
+            Button {
+                // 클릭 시 SearchTagView Sheet 띄워주기
+                isShowTagSearch.toggle()
+            } label: {
+                Text("술 태그 추가하기")
+                    .font(.medium20)
+                    .foregroundStyle(.mainAccent03)
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 10)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(style: StrokeStyle())
+                            .tint(.mainAccent03)
+                    }
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 10)
+
 
         }
     }
