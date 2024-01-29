3d685f7d17ba489b57d7253fb2f18cf2ed4e0b47
백대홍 <bdh3620@naver.com>
2024-01-29 14:53:14 +0900

    Feat:MainView add #36

---
+++ FinalHrmiProjects/View/Main/TodaySulView.swift
@@ -0,0 +1,18 @@
+//
+//  TodaySulView.swift
+//  FinalHrmiProjects
+//
+//  Created by 백대홍 on 1/29/24.
+//
+
+import SwiftUI
+
+struct TodaySulView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodaySulView()
+}
---
+++ FinalHrmiProjects/View/Main/WeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherView.swift
+//  FinalHrmiProjects
+//
+//  Created by 백대홍 on 1/29/24.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
