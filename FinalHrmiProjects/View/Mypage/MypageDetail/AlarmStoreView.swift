70e74872edc5340f8434b11a5905471c51973fa2
SAY-Hong <bobseahee@gmail.com>
2024-01-30 10:56:00 +0900

    [Add] MypageDetail 그룹 생성 #25

---
+++ FinalHrmiProjects/View/Mypage/MypageDetail/AlarmStoreView.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmStoreView.swift
+//  FinalHrmiProjects
+//
+//  Created by 홍세희 on 2024/01/30.
+//
+
+import SwiftUI
+
+struct AlarmStoreView: View {
+    var body: some View {
+        Text("사용자에게 온 알람을 볼 수 있는 뷰입니다. ")
+    }
+}
+
+#Preview {
+    AlarmStoreView()
+}
