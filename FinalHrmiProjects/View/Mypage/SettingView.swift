bdd1ecc9f40e7e42c5d70d22b56d724c061ade05
SAY-Hong <bobseahee@gmail.com>
2024-01-30 10:57:49 +0900

    [Add] SettingView 생성 #25

---
+++ FinalHrmiProjects/View/Mypage/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  FinalHrmiProjects
+//
+//  Created by 홍세희 on 2024/01/30.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
