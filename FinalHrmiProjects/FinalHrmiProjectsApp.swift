2a8fb4e100fcb5eb3e64c3bf5bffc0c11aa57a45
withseon <dlstjsvv@gmail.com>
2024-01-30 11:49:47 +0900

    [Edit] FinalHrmiProjectApp 내 WindowGroup 코드 원상복귀 #31

--- FinalHrmiProjects/FinalHrmiProjectsApp.swift
+++ FinalHrmiProjects/FinalHrmiProjectsApp.swift
@@ -22,8 +22,7 @@ struct FinalHrmiProjectsApp: App {
 //    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-//            ContentView()
-            AddTagView()
+            ContentView()
         }
     }
 }
