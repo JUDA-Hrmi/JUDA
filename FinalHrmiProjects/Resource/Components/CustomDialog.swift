4e56383583a965f6a27db15c5de3bb7673793c11
백대홍 <bdh3620@naver.com>
2024-01-30 14:09:56 +0900

    zsh:1: command not found: q
    zsh:1: command not found: q

---
+++ FinalHrmiProjects/Resource/Components/CustomDialog.swift
@@ -0,0 +1,18 @@
+//
+//  CustomDialog.swift
+//  FinalHrmiProjects
+//
+//  Created by 백대홍 on 1/30/24.
+//
+
+import SwiftUI
+
+struct CustomDialog: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomDialog()
+}
