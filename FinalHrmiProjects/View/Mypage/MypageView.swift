28e3a98809955797f07211e87b683857a249fec6
Phangg <leeckdwns9305@gmail.com>
2024-01-31 00:30:50 +0900

    [Edit] MypageView 수정 리셋 #61

--- FinalHrmiProjects/View/Mypage/MypageView.swift
+++ FinalHrmiProjects/View/Mypage/MypageView.swift
@@ -9,15 +9,7 @@ import SwiftUI
 
 struct MypageView: View {
     var body: some View {
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        NavigationStack {
-            NavigationLink(value: "") {
-                Text("이동")
-            }
-            .navigationDestination(for: String.self) { _ in
-                AlarmStoreView()
-            }
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
