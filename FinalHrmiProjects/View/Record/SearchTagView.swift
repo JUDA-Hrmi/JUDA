066b3b0ab3282a0896e74a933c0b3422e966f5fb
withseon <dlstjsvv@gmail.com>
2024-01-30 10:45:10 +0900

    [Add] 찜 목록이 없을 때 보여주는 Text, 있을 때 보여주는 DrinkListCell ScrollView 추가 #53

--- FinalHrmiProjects/View/Record/SearchTagView.swift
+++ FinalHrmiProjects/View/Record/SearchTagView.swift
@@ -7,9 +7,13 @@
 
 import SwiftUI
 
+// Defualt로 찜 목록 보여주기
 struct SearchTagView: View {
+    // TODO: 데이터 타입 변경 필요
     // Sheet 상태 변수
     @Binding var isShowTagSearch: Bool
+    // 찜 목록 가져오는 배열
+    private let likeds: [String] = []
 
     var body: some View {
         VStack {
@@ -28,6 +32,23 @@ struct SearchTagView: View {
                 }
             }
             .padding(.top, 20)
+            
+            // 찜 목록이 없을 때, 임의의 텍스트 보여주기
+            if likeds.isEmpty {
+                Spacer()
+                Text("술찜 없을 때 텍스트")
+                    .font(.regular14)
+                    .foregroundStyle(.gray01)
+                Spacer()
+            } else {
+                // 찜 목록이 있을 때, DrinkListCell 리스트로 보여주기
+                ScrollView {
+                    ForEach(likeds, id: \.self) { liked in
+                        DrinkListCell()
+                    }
+                }
+            }
+
         }
     }
 }
