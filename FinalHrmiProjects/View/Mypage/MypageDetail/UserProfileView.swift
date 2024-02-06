67df6e07ea53ef9ffd73e92f671b9b56630ad1ee
Phangg <leeckdwns9305@gmail.com>
2024-02-06 14:24:04 +0900

    [Edit] UIKit PHPicker -> SwiftUI PhotosPicker #98

--- FinalHrmiProjects/View/Mypage/MypageDetail/UserProfileView.swift
+++ FinalHrmiProjects/View/Mypage/MypageDetail/UserProfileView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 enum UserType {
     case user, otheruser
@@ -14,20 +15,24 @@ enum UserType {
 struct UserProfileView: View {
     @State private var userNickName: String = "sayHong" // 사용자 닉네임
     @State private var isLibraryPresented: Bool = false // PhotoPicker - 라이브러리에서 선택
+    
     let userType: UserType
-    @State private var userProfilePhoto: UIImage? // 사용자 프로필 이미지
+    
+    @State private var selectedPhotos: [PhotosPickerItem] = []
+    @State private var userProfileImage: UIImage? // 사용자 프로필 이미지
+    
     var body: some View {
         HStack {
             HStack(spacing: 20) {
                 HStack(alignment: .bottom, spacing: -15) {
                     // 사용자 프로필 이미지
-                    if let image = userProfilePhoto { // 사용자 지정 이미지가 있을 때 (이미지 선택 완료했을 경우)
+                    if let image = userProfileImage { // 사용자 지정 이미지가 있을 때 (이미지 선택 완료했을 경우)
                         Image(uiImage: image)
                             .resizable()
                             .aspectRatio(contentMode: .fill)
                             .clipShape(Circle())
                             .frame(width: 70, height: 70)  // 원의 크기 조절
-                            .overlay(Circle().stroke(Color.gray01, lineWidth: 2))  // 원 테두리 추가
+                            .overlay(Circle().stroke(Color.gray03, lineWidth: 1))  // 원 테두리 추가
                     } else {
                         // 사용자 지정 이미지가 없을 때 기본 이미지로 설정
                         // TODO: 기본 이미지 파일 지정해서 넣기
@@ -36,28 +41,22 @@ struct UserProfileView: View {
                             .aspectRatio(contentMode: .fill)
                             .clipShape(Circle())
                             .frame(width: 70, height: 70)
-                            .overlay(Circle().stroke(Color.gray01, lineWidth: 2))
+                            .overlay(Circle().stroke(Color.gray03, lineWidth: 1))
                     }
                     // 프로필 사진 수정 버튼
                     if userType == .user {
-                        Button(action: {
-                            isLibraryPresented.toggle()
-                        }, label: {
+                        LibraryPhotosPicker(selectedPhotos: $selectedPhotos, maxSelectionCount: 1) { // 최대 1장
                             Image(systemName: "pencil.circle.fill")
                                 .resizable()
                                 .aspectRatio(contentMode: .fill)
                                 .frame(width: 20, height: 20)
                                 .foregroundStyle(.gray01)
-                        })
-                        .fullScreenCover(isPresented: $isLibraryPresented) {
-                            ProfilePhotoPicker(selectedPhoto: $userProfilePhoto,
-                                               isLibraryPresented: $isLibraryPresented)
+                        }
+                        .task(id: selectedPhotos) {
+                            await updateImage()
                         }
                         .tint(.mainBlack)
-                    } else {
-                        
                     }
-              
                 }
                 // MARK: 사용자 닉네임 표시
                 Text(userNickName)
@@ -77,6 +76,22 @@ struct UserProfileView: View {
         }
         .padding(.horizontal, 20)
         .padding(.vertical, 10)
+    }
+    
+    @MainActor
+    private func updateImage() async {
+        guard let selectedPhoto = selectedPhotos.first else {
+            return
+        }
+        do {
+            guard let data = try await selectedPhoto.loadTransferable(type: Data.self) else {
+                print("Error photo to data")
+                return
+            }
+            userProfileImage = UIImage(data: data)
+        } catch let error {
+            print("Error loading image: \(error.localizedDescription)")
+        }
     }
 }
 
--- FinalHrmiProjects/View/Record/PhotoSelectPagingTab.swift
+++ FinalHrmiProjects/View/Record/PhotoSelectPagingTab.swift
@@ -15,98 +15,126 @@ struct PhotoData: Identifiable {
 }
 
 struct PhotoSelectPagingTab: View {
-    // photo picker sheet 프로퍼티
-    @State private var isLibraryPresented = false
     // 현재 선택된 탭의 인덱스. 초기값 0
     @State private var selectedIndex = 0
     // photo picker로 선택된 이미지 배열
-    @Binding var selectedPhotos: [UIImage?]
+    @Binding var images: [UIImage?]
+    // photo picker로 선택된 사진 배열
+    @Binding var selectedPhotos: [PhotosPickerItem]
+    let imageSize: CGFloat
     
     var body: some View {
-        // MARK: 해당 부분 삭제 논의
         VStack(alignment: .leading) {
-            HStack(alignment: .lastTextBaseline) {
+            HStack(alignment: .lastTextBaseline, spacing: 6) {
                 Text("사진 등록")
                     .font(.regular16)
                 Text("(최대 10장)")
                     .font(.regular14)
                     .foregroundStyle(.gray01)
             }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 10)
 
             HStack {
                 // 선택된 사진들을 탭뷰 페이징 형식으로 보여주기
                 TabView(selection: $selectedIndex) {
-                    ForEach($selectedPhotos.indices, id: \.self) { index in
-                        // selectedPhotos의 요소가 nil이 아닌 경우, 이미지 보여주기
-                        if selectedPhotos[index] != nil {
-                            ImageView(image: $selectedPhotos[index],
+                    Group {
+                        ForEach($images.indices, id: \.self) { index in
+                            ImageView(images: $images,
                                       selectedPhotos: $selectedPhotos,
-                                      currentIndex: index)
+                                      currentIndex: index,
+                                      imageSize: imageSize)
                             .tag(index)
                         }
-                    }
-                    // selectedPhotos에 사진을 선택할 수 있는 잔여 공간이 남아있는 경우, + 버튼 활성화
-                    if selectedPhotos.contains(nil) {
-                        Image(systemName: "plus.circle.fill")
-                            .font(.largeTitle)
-                            .frame(width: 350, height: 350)
-                            .background(.gray06)
-                            .tag(selectedPhotos.endIndex + 1)
-                            .onTapGesture {
-                                isLibraryPresented.toggle()
+                        // images에 사진을 선택할 수 있는 잔여 공간이 남아있는 경우, + 버튼 활성화
+                        if images.contains(nil) {
+                            LibraryPhotosPicker(selectedPhotos: $selectedPhotos, maxSelectionCount: 10) { // 최대 10장
+                                Image(systemName: "plus.circle.fill")
+                                    .font(.largeTitle)
+                                    .frame(width: imageSize, height: imageSize)
+                                    .background(.gray06)
+                                
                             }
+                            .tag(selectedPhotos.endIndex + 1)
+                        }
+                    }
+                    .task(id: selectedPhotos) {
+                        await updateImages()
+                    }
+                    .onChange(of: images) { _ in
+                        updateSelectedIndex()
                     }
                 }
-                .frame(height: 350)
-                .tabViewStyle(.page)
+                .frame(height: imageSize)
+                .tabViewStyle(.page(indexDisplayMode: .automatic))
             }
         }
-        .fullScreenCover(isPresented: $isLibraryPresented) {
-            let remainingSpaces = getRemainigSpaces()
-            PhotoPicker(selectedPhotos: $selectedPhotos,
-                        isLibraryPresented: $isLibraryPresented,
-                        remainingSpaces: remainingSpaces)
-        }
 		.tint(.mainBlack)
-        // selectedPhotos에 사진이 추가될 때, 탭뷰 selectedIndex를 마지막 사진의 인덱스로 변경
-        .onChange(of: selectedPhotos) { newValue in
-            if let index = newValue.lastIndex(where: { $0 != nil }) {
-                selectedIndex = index
+    }
+    
+    @MainActor
+    private func updateImages() async {
+        guard !selectedPhotos.isEmpty else {
+            images = Array(repeating: nil, count: 10)
+            return
+        }
+        for index in 0..<images.count {
+            if selectedPhotos.count > index {
+                do {
+                    let selectedPhoto = selectedPhotos[index]
+                    guard let data = try await selectedPhoto.loadTransferable(type: Data.self) else {
+                        print("Error photo to data")
+                        return
+                    }
+                    let image = UIImage(data: data)
+                    images[index] = image
+                } catch let error {
+                    print("Error loading image: \(error.localizedDescription)")
+                }
+            } else {
+                images[index] = nil
             }
         }
     }
     
-    private func getRemainigSpaces() -> Int {
-        return selectedPhotos.filter { $0 == nil }.count
+    @MainActor
+    private func updateSelectedIndex() {
+        selectedIndex = images.lastIndex(where: { $0 != nil }) ?? 0
     }
 }
 
 struct ImageView: View {
-    @Binding var image: UIImage?
-    @Binding var selectedPhotos: [UIImage?]
-    var currentIndex: Int
+    // 이미지
+    @Binding var images: [UIImage?]
+    // 사진
+    @Binding var selectedPhotos: [PhotosPickerItem]
+    let currentIndex: Int
+    let imageSize: CGFloat
     
     var body: some View {
-        Image(uiImage: image!)
-            .resizable()
-            .aspectRatio(contentMode: .fill)
-            // TODO: frame 수정
-            .frame(width: 350, height: 350)
-            .clipped()
-            .overlay(alignment: .topTrailing) {
-                Button {
-                    removePhoto()
-                } label: {
-                    XmarkOnGrayCircle()
-                        .font(.title2)
-                        .padding([.top, .trailing], 10)
+        if let image = images[currentIndex] {
+            Image(uiImage: image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: imageSize, height: imageSize)
+                .clipped()
+                .overlay(alignment: .topTrailing) {
+                    Button {
+                        removePhoto()
+                    } label: {
+                        XmarkOnGrayCircle()
+                            .font(.title2)
+                            .padding([.top, .trailing], 10)
+                    }
                 }
-            }
+        }
     }
     
+    @MainActor
     private func removePhoto() {
+        self.images.remove(at: currentIndex)
         self.selectedPhotos.remove(at: currentIndex)
-        selectedPhotos.append(nil)
+        images.append(nil)
     }
 }
 
