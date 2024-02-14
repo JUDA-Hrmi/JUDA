bb159b71b0f57c4090edcf6557ddf13f00971f32
백대홍 <bdh3620@naver.com>
2024-02-15 01:50:50 +0900

    [Edit]: onAppear code change #107

--- JUDA/View/DrinkInfo/DrinkDetail/WellMatched.swift
+++ JUDA/View/DrinkInfo/DrinkDetail/WellMatched.swift
@@ -1,42 +1,54 @@
-//
-//  WellMatched.swift
-//  JUDA
-//
-//  Created by phang on 1/30/24.
-//
-
 import SwiftUI
 
 struct WellMatched: View {
     @EnvironmentObject var aiWellMatchViewModel: AiWellMatchViewModel
     let drink = [
-        "하이네켄",
-        "막걸리",
-        "진로 소주",
-        "발베니",
-        "맥켈란"
+        "트리폴라 피에몬테 로쏘"
     ]
     
     var body: some View {
-        VStack {
-            VStack(alignment: .leading, spacing: 16) {
-                Text("Well Matched")
-                
-                Text(aiWellMatchViewModel.respond)
-                
+        VStack(alignment: .leading, spacing: 10) {
+            // Well Matched
+            HStack(alignment: .lastTextBaseline, spacing: 10) {
+                Text("잘 어울리는 음식")
+                    .font(.semibold18)
+                Text("AI 추천 ✨")
+                    .font(.semibold16)
+                    .foregroundStyle(.mainAccent05)
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.horizontal, 20)
-            .padding(.vertical, 10)
+            // 추천 받은 음식
+            HStack(alignment: .center, spacing: 16) {
+                if let wellMatchedResponse = UserDefaults.standard.string(forKey: "WellMatchedResponse") {
+                    Text("\(wellMatchedResponse)")
+                    
+                } else {
+                    Text(aiWellMatchViewModel.respond)
+                }
+            }
         }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal, 20)
+        .padding(.vertical, 10)
         .onAppear {
             Task {
                 do {
-                    try await aiWellMatchViewModel.request(prompt: "Please recommend 3 foods that go well with you. Only food except drinks. List below --- Beverages List: \(drink)")
+                    aiWellMatchViewModel.respond = try await aiWellMatchViewModel.request(prompt: "Please recommend 3 foods that go well with you. Only food except drinks. List below --- Beverages List: \(drink)")
+                    print("\(aiWellMatchViewModel.respond)")
+                    
+                    // Save the response to UserDefaults
+                    UserDefaults.standard.set(aiWellMatchViewModel.respond, forKey: "WellMatchedResponse")
                 } catch {
                     print("Error fetching recommendations: \(error)")
                 }
             }
+            
+            // Retrieve the saved response from UserDefaults
+            if let wellMatchedResponse = UserDefaults.standard.string(forKey: "WellMatchedResponse") {
+                // Use the retrieved response if needed
+                print("WellMatched Response: \(wellMatchedResponse)")
+            }
+            
+            print("onappear call")
         }
     }
 }
