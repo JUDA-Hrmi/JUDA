831e12d92d35631dc638ced67f47d7b7251b8ed2
백대홍 <bdh3620@naver.com>
2024-02-26 00:05:00 +0900

    [Feat]PostTopView 추가 #111

--- JUDA/View/Main/PostTopView.swift
+++ JUDA/View/Main/PostTopView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct PostTopView: View {
+    @State private var postSearchText = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 24) {
+            HStack {
+                Text("술장 TOP3")
+                    .font(.semibold18)
+                
+                Spacer()
+                
+                NavigationLink {
+                    PostsView(postSearchText: $postSearchText)
+                } label: {
+                    Text("더보기")
+                        .foregroundStyle(.gray01)
+                        .font(.semibold14)
+                }
+            }
+            Rectangle()
+                .frame(height: 138)
+                .foregroundStyle(.gray)
+        }
     }
 }
 
