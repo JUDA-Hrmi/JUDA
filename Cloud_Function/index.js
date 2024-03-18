52d53f30c120d8ddc64d96f4b04f51a22e60d5e3
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-03-18 21:58:51 +0900

    [Add] js로 Cloud Function 다시 init 및 deploy test 완료 #165

---
+++ Cloud_Function/index.js
@@ -0,0 +1,15 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+const {logger} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/v2/https");
+// const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+
+// The Firebase Admin SDK to access Firestore.
+const {initializeApp} = require("firebase-admin/app");
+// const {getFirestore} = require("firebase-admin/firestore");
+
+initializeApp();
+
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
