6b38d3914223a22e03c1362f50792cf68a827cae
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-03-20 23:06:33 +0900

    [Edit] post update trigeer 함수 작성 및 배포 시도 #165

--- Cloud_Function/index.js
+++ Cloud_Function/index.js
@@ -4,7 +4,7 @@ const {onRequest} = require("firebase-functions/v2/https");
 
 // The Firebase Admin SDK to access Firestore.
 const {initializeApp} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore");
+const {getFirestore, FieldValue} = require("firebase-admin/firestore");
 
 import { event } from "firebase-functions/v1/analytics";
 import {
@@ -28,6 +28,7 @@ exports.helloWorld = onRequest((request, response) => {
 const usersString = "users"; // users collection name
 const postsString = "posts"; // posts collection name
 const drinksString = "drinks"; // drinks collection name
+const taggedPostsString = "taggedPostsString"; // taggedPosts collection name
 
 // post 업로드
 // root collection인 posts에 post가 업로드됐을 때
@@ -56,14 +57,18 @@ exports.onPostCreate = onDocumentCreated("posts/{postId}", async (event) => {
 
 // drinks 컬렉션의 taggedPosts 하위 컬렉션에 포스트 필드 데이터 업로드
 async function uploadPostIdToTaggedPost(drinkId, postId, postFieldData) {
-  const taggedPostsString = "taggedPostsString"; // taggedPosts collection name
-
   const drinkRef = db.collection(drinksString).doc(drinkId);
-  const taggedPostRef = drinkRef.collection(taggedPostsString).doc(postId);
-  try {
-    await taggedPostRef.set(postFieldData);    
-  } catch (error) {
-    console.error(`error :: uploadPostIdToTaggedPost(), drinkId: ${drinkId}, postId: ${postId}`);
+
+  if ((await drinkRef.get()).exists) {
+    const taggedPostRef = drinkRef.collection(taggedPostsString).doc(postId);
+    try {
+      await taggedPostRef.set(postFieldData);
+      console.log(`success upload post :: uploadPostIdToTaggedPost(), drinkId: ${drinkId}, postId: ${postId}`);
+    } catch (error) {
+      console.error(`error :: uploadPostIdToTaggedPost(), drinkId: ${drinkId}, postId: ${postId}`, error);
+    }
+  } else {
+    console.log(`drink document not exist for drinkId: ${drinkId}`);
   }
 }
 
@@ -71,10 +76,12 @@ async function uploadPostIdToTaggedPost(drinkId, postId, postFieldData) {
 async function uploadPostToUsers(userId, postId, postFieldData) {
   const userRef = db.collection(usersString).doc(userId);
   const postRef = userRef.collection(postsString).doc(postId);
+
   try {
-    await postRef.set(postFieldData); 
+    await postRef.set(postFieldData);
+    console.log(`success upload post :: uploadPostToUsers(), userId: ${userId}, postId: ${postId}`);
   } catch (error) {
-    console.error(`error :: uploadPostToUsers(), userId: ${userId}, postId: ${postId}`);
+    console.error(`error :: uploadPostToUsers(), userId: ${userId}, postId: ${postId}`, error);
   }
 }
 
@@ -89,21 +96,39 @@ exports.onPostUpdate = onDocumentUpdated("posts/{postId}", async (event) => {
   const user = postFieldData.user;
   const userId = user.userID;
 
-  // # 사용자의 'posts' 컬렉션 내 해당 포스트 업데이트
-  // # 사용자의 'likedPosts' 컬렉션 내 해당 포스트 업데이트
-  // # 사용자의 notification 중 'likedPosts' 컬렉션 내 해당 포스트 업데이트
-  // # 'drinks' 컬렉션 내 'taggedPosts'에서 해당 포스트 업데이트
-  
+  await Promise.all(
+    // # 사용자의 'posts' 컬렉션 내 해당 포스트 업데이트
+    updateUserPost(userId, postId, postFieldData),
+    // # 사용자의 'likedPosts' 컬렉션 내 해당 포스트 업데이트
+    updateUserLikedPosts(userId, postId, postFieldData),
+    // # 사용자의 notification 중 'likedPosts' 컬렉션 내 해당 포스트 업데이트
+    updateNotificationLikedPosts(userId, postId, postFieldData),
+    // # 'drinks' 컬렉션 내 'taggedPosts'에서 해당 포스트 업데이트
+    updateTaggedPostDrinks(postId, postFieldData)
+  )
 });
 
 // # 사용자의 'posts' 컬렉션 내 해당 포스트 업데이트
 async function updateUserPost(userId, postId, postFieldData) {
   const userRef = db.collection(usersString).doc(userId);
   const userPostRef = userRef.collection(postsString).doc(postId);
-  try {
-    await userPostRef.update(postFieldData); 
-  } catch (error) {
-   console.error(`error :: updateUserPost(), userId: ${userId}, postId: ${postId}`); 
+
+  if ((await userPostRef.get()).exists) { // user의 하위 posts collection에 해당 post 존재 O
+    try {
+      await userPostRef.update(postFieldData); 
+      console.log(`success update post :: updateUserPost(), userId: ${userId}, postId: ${postId}`);
+    } catch (error) {
+     console.error(`error :: updateUserPost(), userId: ${userId}, postId: ${postId}`, error); 
+    }
+  } else { // user의 하위 collection에 해당 post 존재 X
+    console.log(`user(userId: ${userId})'s sub collection(posts) not exist post(postId: ${postId})`);
+
+    try {
+      await userPostRef.set(postFieldData);
+      console.log(`success upload post :: updateUserPost(), userId: ${userId}, postId: ${postId}`);
+    } catch (error) {
+      console.error(`error :: upload post user's posts, userId: ${userId}, postId: ${postId}`, error);
+    }
   }
 }
 
@@ -112,13 +137,78 @@ async function updateUserLikedPosts(userId, postId, postFieldData) {
   const likedPostsString = "likedPosts";
   const userRef = db.collection(usersString).doc(userId);
   const userLikedPostRef = userRef.collection(likedPostsString).doc(postId);
-  try {
-    await userLikedPostRef.update(postFieldData); 
-  } catch (error) {
-    console.error(`error :: updateUserLikedPosts(), userId: ${userId}, postId: ${postId}`);
+  
+  if((await userLikedPostRef.get()).exists) { // user의 하위 likedPosts collection에 해당 post 존재 O
+    try {
+      await userLikedPostRef.update(postFieldData); 
+      console.log(`success update post :: updateUserLikedPosts(), userId: ${userId}, postId: ${postId}`);
+    } catch (error) {
+      console.error(`error :: updateUserLikedPosts(), userId: ${userId}, postId: ${postId}`, error);
+    }
+  } else { // user의 하위 likedPosts collection에 해당 post 존재 X
+    console.log(`user(userId: ${userId})'s sub collection(likedPosts) not exist post(postId: ${postId})`);
+
+    try {
+      await userLikedPostRef.set(postFieldData);
+      console.log(`success upload post :: updateUserLikedPosts(), userId: ${userId}, postId: ${postId}`);
+    } catch (error) {
+      console.error(`error :: upload post likedPosts, userId: ${userId}, postId: ${postId}`, error);
+    }
   }
 }
 
 // # 사용자의 notification 중 'likedPosts' 컬렉션 내 해당 포스트 업데이트
+async function updateNotificationLikedPosts(userId, postId, postFieldData) {
+  const notificationsString = "notifications";
+  const notificationId = userId + postId; // notification document id
+
+  const userRef = db.collection(usersString).doc(userId);
+  const notificationRef = userRef.collection(notificationsString).doc(notificationId);
+  if((await notificationRef.get()).exists) { // user의 하위 notification collection에 해당 post 존재 O
+    try {
+      await notificationRef.update(postFieldData);
+      console.log(`success update post :: updateNotificationLikedPosts(), userId: ${userId}, postId: ${postId}`);
+    } catch (error) {
+      console.error(`error :: updateNotificationLikedPosts(), userId: ${userId}, postId: ${postId}`), error;
+    }
+  } else { // user의 하위 notifications collection에 해당 post 존재 X
+    console.log(`user(userId: ${userId})'s sub collection(${notificationsString}) not exist post(postId: ${postId})`);
+
+    try {
+      await notificationRef.set(postFieldData);
+      console.log(`success upload post :: updateNotificationLikedPosts(), userId: ${userId}, postId: ${postId}`);
+    } catch (error) {
+      console.error(`error :: upload post ${notificationsString}, userId: ${userId}, postId: ${postId}`, error);
+    }
+  }
+}
 
 // # 'drinks' 컬렉션 내 'taggedPosts'에서 해당 포스트 업데이트
+async function updateTaggedPostDrinks(postId, postFieldData) {
+  const drinksRef = db.collection(drinksString);
+  const drinkTags = postFieldData.drinkTags;
+
+  for (const drinkTag of drinkTags) {
+    const drinkId = drinkTag.drinkID;
+    const drinkRef = drinksRef.doc(drinkId);
+    const taggedPostRef = drinkRef.collection(taggedPostsString).doc(postId);
+
+    if((await taggedPostRef.get()).exists) { // drink의 하위 taggedPosts collection에 해당 post 존재 O
+      try {
+        await taggedPostRef.update(postFieldData);
+        console.log(`success update post :: updateTaggedPostDrinks(), drinkId: ${drinkId}, postId: ${postId}`);
+      } catch (error) {
+        console.error(`error :: updateTaggedPostDrinks(), drinkId: ${drinkId}, postId: ${postId}`, error);
+      }
+    } else {  // drink의 하위 taggedPosts collection에 해당 post 존재 X
+      console.log(`drink(drinkId: ${drinkId})'s sub collection(${taggedPostsString}) not exist post(postId: ${postId})`);
+
+      try {
+        await taggedPostRef.set(postFieldData);
+        console.log(`success upload post :: updateTaggedPostDrinks(), drinkId: ${drinkId}, postId: ${postId}`);
+      } catch (error) {
+        console.error(`error :: upload post ${taggedPostsString}, drinkId: ${drinkId}, postId: ${postId}`, error);
+      }
+    }
+  }
+}
