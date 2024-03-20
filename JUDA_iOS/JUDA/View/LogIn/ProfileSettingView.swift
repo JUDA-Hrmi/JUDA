bb90d2de9dbd0133e36e83fef509dfa05373a7a5
Phangg <leeckdwns9305@gmail.com>
2024-03-20 15:54:53 +0900

    [Edit] 회원가입 완료 버튼 로직 수정 #166
    회원 가입 시, 완료 버튼 누르면
    signInDoneButtonTapped() 메서드 실행
    - 메서드 내부에서 애플 / 구글 확인하여 각각에 맞는 로직 수행하도록 수정

--- JUDA_iOS/JUDA/View/LogIn/ProfileSettingView.swift
+++ JUDA_iOS/JUDA/View/LogIn/ProfileSettingView.swift
@@ -84,7 +84,7 @@ struct ProfileSettingView: View {
                         .onChange(of: selectedPhotos) { _ in
                             Task {
                                 do {
-                                    try await updateImage()
+                                    userProfileImage = try await authViewModel.updateImage(selectedPhotos: selectedPhotos)
                                 } catch {
                                     // 이미지 로드 실패 alert 띄워주기
                                     isShowAlertDialog = true
@@ -183,28 +183,16 @@ struct ProfileSettingView: View {
                 // "완료" 버튼
                 Button {
                     Task {
-                        // 애플의 경우 로그아웃이 된 상태라, 애플인지 아닌지 체크를 google 이 아닌 경우로 체크
-                        if authViewModel.currentUser?.userField.authProviders != AuthProviderOption.google.rawValue {
-                            // 재로그인
-                            let signWithApple = SignInWithAppleHelper()
-                            let appleIDCredential = try await signWithApple()
-                            authViewModel.isLoading = true
-                            await authViewModel.signInApple(appleIDCredential: appleIDCredential)
-                            authViewModel.signInStatus = true
-                        }
-                        // 프로필 이미지 storage 저장
-                        await authViewModel.uploadProfileImageToStorage(image: userProfileImage)
-                        // 유저 이름, 생일, 성별, 프로필, 알림 동의 등 forestore 에 저장
-                        authViewModel.addUserDataToStore(
-                            name: name,
-                            age: Formatter.calculateAge(birthdate: birthDate) ?? 20,
-                            gender: selectedGender!.rawValue,
-                            notification: notificationAllowed
+                        await authViewModel
+                            .signInDoneButtonTapped(
+                                name: name,
+                                age: Formatter.calculateAge(birthdate: birthDate) ?? 20,
+                                profileImage: userProfileImage,
+                                gender: selectedGender!.rawValue,
+                                notification: notificationAllowed
                             )
-                        // 유저 데이터 받기
-                        await authViewModel.getCurrentUser()
+                        authViewModel.isLoading = false
                     }
-                    authViewModel.isLoading = false
                     navigationRouter.back()
                 } label: {
                     Text("완료")
@@ -247,19 +235,5 @@ struct ProfileSettingView: View {
         }
         // 회원 가입 시, 로딩 뷰
         .loadingView($authViewModel.isLoading)
-    }
-    
-    private func updateImage() async throws {
-        guard let selectedPhoto = selectedPhotos.first else {
-            return
-        }
-        do {
-            guard let data = try await selectedPhoto.loadTransferable(type: Data.self) else {
-                throw PhotosPickerImageLoadingError.invalidImageData
-            }
-            userProfileImage = UIImage(data: data)
-        } catch {
-            throw PhotosPickerImageLoadingError.invalidImageData
-        }
     }
 }
